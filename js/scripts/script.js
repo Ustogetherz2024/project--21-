f1622a8ad422375ddd80b97778f41a3a65acaa49
Ustogetherz2024 <anya.krop53@gmail.com>
2025-06-26 12:57:30 +0300

    Update script.js

--- js/scripts/script.js
+++ js/scripts/script.js
@@ -47,4 +47,54 @@ document.addEventListener('DOMContentLoaded', function() {
             clickable: true,
         },
     });
+
+    const coursesListContainer =  document.querySelector('.courses__list');
+    if (coursesListContainer) {
+        const apiUrl = 'data.json';
+
+        const courceItem = (img, title, description, rating, alt, button_text) => {
+        const item = `
+            <div class="courses__item course-card">
+                    <img src="${img}" alt="${alt}" class="course-card__image">
+                    <h3 class="course-card__title">${title}</h3>
+                    <p class="course-card__description">${description}</p>
+                    <span class="course-card__rating">${rating}</span>
+                    <a href="#" class="course-card__link button button--secondary">${button_text}</a>
+                </div>
+        `
+
+        return item
+    }
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Данные
+            console.log(typeof (data)); // Тип полученных данных
+            data.forEach(item => {
+                const cource = courceItem(item.img, item.title, item.description, item.rating, item.alt, item.button_text);
+                coursesListContainer.insertAdjacentHTML('beforeend', cource);
+            });
+        })  
+        .catch(error => {
+            console.error('Ошибка при загрузке данных:', error);
+        });
+    }
 });
+
+// Preloader страницы
+const preloader = document.querySelector('.preloader');
+const content = document.querySelector('.content');
+if (preloader && content) {
+    setTimeout(() => {
+        // Скрываем прелоадер
+        preloader.style.opacity = '0';
+        preloader.style.visibility = 'hidden';
+
+        // Показываем контент
+        content.style.display = 'block';
+
+        // Удаляем элемент из DOM
+        preloader.remove();
+    }, 3000); // Задержка 3 секунды
+}
